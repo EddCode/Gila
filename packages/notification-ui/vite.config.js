191a672740195d77ec4a119ec3ceb81d02277675
Edgar figueroa <edgar.figueroa.gtz@gmail.com>
2023-07-04 12:58:20 -0600

    Fix vite acces through docker container

--- packages/notification-ui/vite.config.js
+++ packages/notification-ui/vite.config.js
@@ -2,13 +2,17 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
     port: 4000
   },
   resolve: {
     alias: {
       '@': '/src'
     }
-  },
-  plugins: [react()]
+  }
 })
